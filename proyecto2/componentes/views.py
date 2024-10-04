0f5bccc8ad1f8d282239a3e62f749822feb73a20
Carlos Perea <carlos.perea.figuera.al@iespoligonosur.org>
2024-10-04 14:40:51 +0200

    Acabado

--- proyecto2/componentes/views.py
+++ proyecto2/componentes/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import cpu
+from .models import gpu
+from.models import memory
 
 # Create your views here.
 def cpu_list(request):
-    return render(request, 'cpu_list.html', {})
+    cpus = cpu.objects.all()
+    return render(request, 'cpu_list.html', {"cpu_mostrar": cpus})
 
 def gpu_list(request):
-    return render(request, 'gpu_list.html'  , {})
+    gpus = gpu.objects.all()
+    return render(request, 'gpu_list.html'  , {"gpu_mostrar" : gpus})
 
 def memory_list(request):
-    return render(request, 'memory_list.html' , {})
+    memorys = memory.objects.all()
+    return render(request, 'memory_list.html' , {"memory_mostrar" : memorys})
