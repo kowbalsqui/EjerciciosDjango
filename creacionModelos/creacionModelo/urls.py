e959d59237b08c3e2e791d26cf515991d464c4fb
Carlos Perea <carlos.perea.figuera.al@iespoligonosur.org>
2024-10-25 14:33:36 +0200

    URL 7

--- creacionModelos/creacionModelo/urls.py
+++ creacionModelos/creacionModelo/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.index, name= 'index'),
     path('Proyectos/listar', views.lista_proyectos, name= 'lista_proyectos'),
     path('Tareas/listar/<int:proyecto_id>', views.lista_tareas, name= 'lista_tareas'),
+    path('UsuarioTarea/listar/<int:tarea_id>', views.asignacionDeTarea, name= 'asignacionDeTarea'),
+    path('Tareas/listarObservaciones/<str:texto>', views.tareaConObervacion, name= 'tareaConObervacion'),
+    path('Tareas/listarEntreAñosyCompletadas/<int:año1>/<int:año2>', views.tareasCompletadaEntreAños, name = "tareasCompletadaEntreAños"),
+    path('Usuario/listarUltimoUsuarioComento')
 ]
--- creacionModelos/creacionModelo/views.py
+++ creacionModelos/creacionModelo/views.py
@@ -11,5 +11,18 @@ def lista_proyectos(request):
     return render(request, 'proyecto/listar_proyectos.html', {"proyectos_mostrar" : proyectos})
 
 def lista_tareas(request, proyecto_id):
-    tareas = Tarea.objects.select_related("proyecto").filter(proyecto_id = proyecto_id)
+    tareas = Tarea.objects.select_related("proyecto").filter(proyecto_id = proyecto_id).order_by("-fechaCreacion")
     return render(request, 'tarea/listar_tareas.html', {"tareas_mostrar" : tareas})
+
+def asignacionDeTarea (request, tarea_id):
+    usuario = Usuario.objects.filter(usuarios_tareas__tarea=tarea_id).order_by()
+    return render(request, 'usuario/listarusuariosConTarea.html', {"usuarioTarea_mostrar": usuario})  
+
+def tareaConObervacion (request, texto):
+    tarea = Tarea.objects.filter(asignaciondetarea__observaciones__contains = texto)
+    return render(request, 'tarea/listar_tareas_observaciones.html', {"tareas_mostrar" : tarea})
+
+def tareasCompletadaEntreAños(request, año1, año2):
+    tareas = Tarea.objects.filter(fechaCreacion__year__gte=año1,fechaCreacion__year__lte=año2, estado = 'Completada').all()
+    return render(request, 'tarea/listar_tareas_completadas_entre_años.html', {"tareas_mostrar" : tareas})
+
